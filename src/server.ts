2d46d8335435fb6ad7b5ce30c00f9c6c01c74a30
Beatriz Oliveira <beatriz30soares@gmail.com>
2021-04-24 01:51:59 +0100

    added second lesson

--- src/server.ts
+++ src/server.ts
@@ -1,29 +1,11 @@
 import express from 'express';
+import './database';
+import { routes } from './routes';
 
 const app = express();
 
-/**
- * GET = Find
- * POST = Create
- * PUT = Change
- * DELETE = Delete
- * PATCH = Change a specific information
- */
+app.use(express.json());
 
-/*
-first parameter the url route, the second a function that receives a request(everything that is coming to my request)
-and the response (everything that will be returned to the user)
-*/
-app.get('/', (request, response) => {
-    return response.json({
-        message: 'Hey NLW 05',
-    });
-});
-
-app.post('/users', (request, responde) => {
-    return responde.json({
-        message: 'User saved successfully',
-    });
-});
+app.use(routes);
 
 app.listen(8080, () => console.log('Server is running on port 8080'));
